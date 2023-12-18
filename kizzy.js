bae46add6097503387f4cf9cab1e98b48ee72af9
wKizzy <154281068+wKizzy@users.noreply.github.com>
2023-12-19 00:49:32 +0300

    Create kizzy.js
    About me

---
+++ kizzy.js
@@ -0,0 +1,9 @@
+const profile = {
+  Name: "Ali",
+  Age: "19",
+  jobs: ["Mechanic","Auto trading"],
+  Interests: ["Coding", "Reading", "Music"],
+  GitHub: "github.com/wKizzy",
+  İnstagram: "@aliewxl",
+  Discord: "kizzyxw"
+};
